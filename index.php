94ebf295aaa8122a264e7bf63a81b83f7eafa2d7
Pazit Schrecker <38817948+pazitschrecker@users.noreply.github.com>
2019-08-18 11:17:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+<script type="text/javascript" src="js/jquery-ui-1.7.2.custom.min.js"></script>
+<script type="text/javascript" src="js/jquery.jeditable.mini.js"></script>
+<script type="text/javascript" src="js/lists.js"></script>
+<script type="text/javascript">
+    initialize();
+</script>
