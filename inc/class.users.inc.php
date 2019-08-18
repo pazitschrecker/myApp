f033043d42e158c1810235e19f42d11635a3f5d4
Pazit Schrecker <38817948+pazitschrecker@users.noreply.github.com>
2019-08-18 11:49:03 -0400

    Update class.users.inc.php

--- inc/class.users.inc.php
+++ inc/class.users.inc.php
@@ -1,4 +1,46 @@
 <?php
+ 
+/**
+ * Handles list interactions within the app
+ *
+ * PHP version 5
+ *
+ * @author Jason Lengstorf
+ * @author Chris Coyier
+ * @copyright 2009 Chris Coyier and Jason Lengstorf
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ *
+ */
+class ColoredListsItems
+{
+   
+      /**
+       * The database object
+       *
+       * @var object
+       */
+   
+       private $_db;
+   
+      /**
+       * Checks for a database object and creates one if none is found
+       *
+       * @param object $db
+       * @return void
+       */
+       public function __construct($db=NULL) {
+           if(is_object($db)) {
+               $this->_db = $db;
+           }
+           else {
+               $dsn = "mysql:host=",DB_HOST.";dbname=.DB_NAME;
+               $this->_db = new PDO($dsn, DB_USER, DB_PASS);
+           }
+       }
+   }
+?>
+
+<?php
  /**
   * Handles user interactions within the app
   *
@@ -10,8 +52,7 @@
   */
   
   class ColoredListsUsers {
-   
-   /**
+    /**
     * Sends a link to a user that lets them reset their password
     * 
     * @param string $email  the user's emaol address
